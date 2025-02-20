1fad7f630b7a60037f4c6c29824836687df3fa87
Ronney <153025062+musasPI@users.noreply.github.com>
2025-02-20 03:21:45 -0300

    refactor: add comments in ContaTerminal.java

--- BancoTerminal/src/ContaTerminal.java
+++ BancoTerminal/src/ContaTerminal.java
@@ -8,12 +8,15 @@ public class ContaTerminal {
             
            
                 System.out.println("Digite seu Nome: ");
+                //Define que a variável de valor String consiga a ler a entrada de dados do usuário no terminal.
                 String NomeCliente = in.next();
            
                 System.out.println("Digite sua Agência: ");
+                //Define que a variável de valor String consiga a ler a entrada de dados do usuário no terminal.
                 String Agencia = in.next();
 
                 System.out.println("Digite o número da sua Conta: ");
+                //Define que a variável de valor int consiga a ler a entrada de dados do usuário no terminal.
                 int numeroConta = in.nextInt();
 
                 System.out.println("Olá Sr(a). " + NomeCliente + " obrigado por criar uma conta em nosso banco, sua Conta é " + numeroConta +  ", Agência " + Agencia + " seu Saldo é de R$ " + saldo + " e já está disponível para saque.");
